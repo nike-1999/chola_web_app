f2e7cd385488f71e8176873def68bb7dde45a61f
nike-1999 <nikhil.gupta@zeddlabz.com>
2024-05-04 02:34:12 +0530

    this is my second change

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,21 @@ function App() {
   };
 
   return (
-    <div className="bg-violet-300 h-screen ">
+    <div className="bg-violet-300 h-screen overflow-hidden">
       <img
         src="./src/assets/logo.svg"
-        className="w-70	h-36"
+        className="w-70	h-32"
         alt="Passenger App"
       />
-      <div className="flex items-center justify-center h-400">
+      <div className="flex items-center justify-center h-80">
         <div className="flex flex-col items-center">
           <img
             src="./src/assets/passenger.svg"
-            className="w-auto	h-96"
+            className="w-auto	h-80"
             alt="Passenger App"
           />
           <button
-            className="bg-black hover:bg-violet-500 hover:transition-colors delay-100 text-white font-bold py-3 px-4 rounded-lg lg:w-[200px] md:w-[200px]"
+            className="bg-black hover:bg-violet-500 hover:transition-colors delay-100 text-white font-bold py-2 px-2 rounded-lg w-[160px]"
             onClick={() =>
               redirectToURL(
                 "https://drive.google.com/file/d/1VhsCNOTvs7Ne6zm0A7fjU91Sw7ovBmtg/view?usp=sharing"
@@ -34,11 +34,11 @@ function App() {
         <div className="flex flex-col items-center">
           <img
             src="./src/assets/driver.svg"
-            className="w-auto h-96"
+            className="w-auto h-80"
             alt="Driver App"
           />
           <button
-            className="bg-black hover:bg-violet-500 hover:transition-colors delay-100 text-white font-bold py-3 px-4 rounded-lg lg:w-[200px] md:w-[200px]"
+            className="bg-black hover:bg-violet-500 hover:transition-colors delay-100 text-white font-bold py-2 px-2 rounded-lg w-[160px]"
             onClick={() =>
               redirectToURL(
                 "https://drive.google.com/file/d/1BvT_DKan_-2HJFw_6beNWZ4gSQX-xOaD/view?usp=sharing"
@@ -50,7 +50,7 @@ function App() {
           </button>
         </div>
       </div>
-      <p className="text-center text-black font-bold py-10 m-auto">
+      <p className="text-center text-black font-bold py-16 m-auto">
         Please click the buttons above to download and install the respective
         <br />
         apps for passengers and drivers.
